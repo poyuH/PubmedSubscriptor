f5b2b3e588f324456c386572cd8d74ea878e510f
Po-Yu Huang <poyu0987@gmail.com>
2020-01-01 23:58:56 -0500

    correct search url and keep search term in session

--- flask/flaskr/controller.py
+++ flask/flaskr/controller.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, redirect, render_template, request, session, flash, url_for, g
 )
+from datetime import datetime
 import functools
 from .model import db, auth, paper
 from .parser import parserController
@@ -15,6 +16,7 @@ STRMS = global_values.User.STRMS.value
 USER_ID = global_values.Session.USER_ID.value
 PAPERS = global_values.SearchTerm.PAPERS.value
 MINDATE = global_values.SearchTerm.MINDATE.value
+CONTEXT = 'context'
 
 # connects to our database
 @bp.before_request
@@ -26,21 +28,31 @@ def before_request():
 def teardown_request(exception):
     my_db.close(exception)
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/', methods=['GET', 'POST'])
 def home_page():
     context = {}
     if request.method == 'POST':
-        search_term = request.form[STRMS]
-        date_after = request.form[MINDATE]
-        context = parserController.parse_search_results(search_term, '2018/08/14')
+        search_term = request.form.get(STRMS)
+        mindate = request.form.get(MINDATE)
+        if mindate:
+            date_after = datetime.strptime(mindate, '%m/%d/%Y').strftime('%Y/%m/%d')
+        else:
+            date_after = "1800/01/01"
+        context = parserController.parse_search_results(search_term, date_after)
+        context[STRMS] = search_term
+        context[MINDATE] = mindate
+        session[CONTEXT] = context
         return render_template("index.html", **context)
     if g.user:
-        search_terms = paper.get_user_search_terms(g.user)
-        context = paper.get_papers(search_terms)
+        context = session.get(CONTEXT)
+        # TODO
+        # search_terms = paper.get_user_search_terms(g.user)
+        # context = paper.get_papers(search_terms)
     return render_template("index.html", **context)
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route('/register', methods=['POST'])
 def register():
+    context = {}
     if request.method == 'POST':
         email = request.form[EMAIL]
         pwd = request.form[PWD]
@@ -55,8 +67,9 @@ def register():
         flash(error)
     return render_template('auth/register.html')
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login', methods=['POST'])
 def login():
+    context = {}
     if request.method == 'POST':
         email = request.form[EMAIL]
         pwd = request.form[PWD]
