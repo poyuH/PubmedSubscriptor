5f32ab53efa9cbba9627aa01791598a5c9c634d4
Po-Yu Huang <poyu0987@gmail.com>
2020-01-04 20:43:17 -0500

    remove redis queue

--- flask/flaskr/parser/parserController.py
+++ flask/flaskr/parser/parserController.py
@@ -1,5 +1,3 @@
-from redis import Redis
-from rq import Queue
 from .parser import pmid_gen, get_metadata
 from .. import global_values
 from collections import defaultdict
@@ -11,35 +9,6 @@ PUB_DATE = global_values.Paper.PUB_DATE.value
 ABSTRACT = global_values.Paper.ABSTRACT.value
 TITLE = global_values.Paper.TITLE.value
 URL = global_values.Database.URL.value
-
-
-def parse_search_results_slow(query, date_after, n=10):
-    """
-    parse up to n pubmed results with published date after date_after
-    """
-    jobs = []
-
-    q = Queue(connection=Redis())
-    for pmid in pmid_gen(query, date_after, n):
-        jobs.append(q.enqueue(get_metadata, pmid))
-
-    while len(q) > 0:
-        continue
-    # TODO better solution?
-    if not jobs[-1].result:
-        time.sleep(2)
-    context = defaultdict(list)
-    for job in jobs:
-        try:
-            context[URL].append('https://www.ncbi.nlm.nih.gov/pubmed/' + job.result.get(PMID))
-            context[TITLE].append(job.result.get(TITLE))
-            context[PUB_DATE].append(job.result.get(PUB_DATE))
-            context[JOURNAL].append(job.result.get(JOURNAL))
-            context[ABSTRACT].append(job.result.get(ABSTRACT))
-        except AttributeError:
-            continue
-    return context
-
 
 def parse_search_results(query, date_after, n=10):
     """
