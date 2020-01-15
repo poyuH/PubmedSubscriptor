23d1c6902917a45b575882203e9fb8e315053a53
Po-Yu Huang <poyu0987@gmail.com>
2020-01-15 08:36:13 +0800

    handle APIrate limit with deque

--- flask/flaskr/parser/parserController.py
+++ flask/flaskr/parser/parserController.py
@@ -1,6 +1,6 @@
-from .parser import pmid_gen, get_metadata
+from .parser import pmid_gen, get_metadata, APIRateLimitError
 from .. import global_values
-from collections import defaultdict
+from collections import defaultdict, deque
 import time
 
 PMID = global_values.Paper.PMID.value
@@ -15,14 +15,36 @@ def parse_search_results(query, date_after, n=10):
     parse up to n pubmed results with published date after date_after
     """
     context = defaultdict(list)
-    for pmid in pmid_gen(query, date_after, n):
-        result_dict = get_metadata(pmid)
-        context[PMID].append(result_dict.get(PMID))
-        context[URL].append('https://www.ncbi.nlm.nih.gov/pubmed/' + result_dict.get(PMID))
-        context[TITLE].append(result_dict.get(TITLE))
-        context[PUB_DATE].append(result_dict.get(PUB_DATE))
-        context[JOURNAL].append(result_dict.get(JOURNAL))
-        context[ABSTRACT].append(result_dict.get(ABSTRACT))
+    q = deque([])
+    while True:
+        try:
+            for pmid in pmid_gen(query, date_after, n):
+                print(pmid)
+                q.append(pmid)
+            break
+        except APIRateLimitError:
+            print('fail')
+            time.sleep(1)
+            continue
+        except:
+            break
+    while q:
+        pmid = q.popleft()
+        print('try', pmid)
+        try:
+            result_dict = get_metadata(pmid)
+            context[PMID].append(result_dict.get(PMID))
+            context[URL].append('https://www.ncbi.nlm.nih.gov/pubmed/' + result_dict.get(PMID))
+            context[TITLE].append(result_dict.get(TITLE))
+            context[PUB_DATE].append(result_dict.get(PUB_DATE))
+            context[JOURNAL].append(result_dict.get(JOURNAL))
+            context[ABSTRACT].append(result_dict.get(ABSTRACT))
+        except APIRateLimitError:
+            print('fail', pmid)
+            time.sleep(1)
+            q.appendleft(pmid)
+        except:
+            break
     return context
 
 if __name__ == '__main__':
